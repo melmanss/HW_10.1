888f7aa1142519021225239ac0bdda3f4b604888
melmanss <74724829+melmanss@users.noreply.github.com>
2025-01-23 21:15:46 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введіть ціле число: ");
         int number = readInt(scanner);
         System.out.println("Квадрат числа " + number + " дорівнює: " + (number * number) + ".");
-
         System.out.print("Введіть радіус циліндра: ");
         double radius = readDouble(scanner);
         System.out.print("Введіть висоту циліндра: ");
         double height = readDouble(scanner);
         System.out.println("Об'єм циліндра з радіусом " + radius + " і висотою " + height +
                 " дорівнює " + (Math.PI * radius * radius * height) + ".");
-
         System.out.print("Введіть кількість елементів масиву: ");
         int n = readInt(scanner);
         int[] numbers = new int[n];
@@ -24,28 +19,23 @@ public class Main {
             numbers[i] = readInt(scanner);
         }
         System.out.println("Сума всіх елементів масиву дорівнює " + sumArray(numbers) + ".");
-
         scanner.nextLine();
         System.out.print("Введіть рядок: ");
         String text = scanner.nextLine();
         System.out.println("Рядок в зворотньому порядку: " + new StringBuilder(text).reverse().toString() + ".");
-
         System.out.print("Введіть a: ");
         int a = readInt(scanner);
         System.out.print("Введіть b: ");
         int b = readInt(scanner);
         System.out.println("Результат " + a + "^" + b + " дорівнює " + Math.pow(a, b) + ".");
-
         System.out.print("Введіть кількість повторень: ");
         int nText = readInt(scanner);
         scanner.nextLine();
         System.out.print("Введіть текстовий рядок: ");
         String textToPrint = scanner.nextLine();
         printTextMultipleTimes(nText, textToPrint);
-
         scanner.close();
     }
-
     private static int sumArray(int[] array) {
         int sum = 0;
         for (int num : array) {
@@ -53,7 +43,6 @@ public class Main {
         }
         return sum;
     }
-
     private static int readInt(Scanner scanner) {
         while (true) {
             try {
@@ -64,7 +53,6 @@ public class Main {
             }
         }
     }
-
     private static double readDouble(Scanner scanner) {
         while (true) {
             try {
@@ -75,7 +63,6 @@ public class Main {
             }
         }
     }
-
     private static void printTextMultipleTimes(int n, String text) {
         for (int i = 0; i < n; i++) {
             System.out.println(text);
